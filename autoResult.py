d9b537c29cdde2045589103ff4f43674ac4546ea
nayshuleralex <nayshuleralex@gmail.com>
2017-05-29 15:46:28 +0300

    some minor changes

--- autoResult.py
+++ autoResult.py
@@ -8,7 +8,6 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore, QtGui
-from PyQt4.QtCore import QStringList
 
 import Main, sys, time, threading, switch, serial.tools.list_ports
 
